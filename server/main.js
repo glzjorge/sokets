90d00d16e3bed26273a23b7414543557fc62195f
Jorge Luis Gonzalez Hernandez <glz_jorge@hotmail.com>
2021-05-31 09:46:32 -0600

    creo la conexion on en sockets

--- server/main.js
+++ server/main.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
     res.status(200).send("hola mundo de sockets");
 });
 
+io.on('connection', function(soket){
+    console.log('Alguien se ha conectado usando sockets')
+})
+
 server.listen(3002, function(){
     console.log("El serevidor esta corriendo en http://localhost:3002")
 });
