be6106eca5b18b6658cd8253e940c5ca1c5330ca
Jorge Luis Gonzalez Hernandez <glz_jorge@hotmail.com>
2021-06-01 10:04:26 -0600

    creamos un template html y hacemos render del mensaje

--- public/main.js
+++ public/main.js
@@ -2,4 +2,14 @@ var socket = io.connect('http://localhost:3002', {'forceNew': true});
 
 socket.on('messages', function(data){
     console.log(data);
+    render(data);
 });
+
+function render(data){
+    var html = `<div>
+                <strong>${data.autor}</strong>:
+                <em>${data.texto}</em>
+                </div>`;
+            
+                document.getElementById('messages').innerHTML = html;
+};
