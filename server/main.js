9e963f9577782f65ec50a3688665598b33157199
Jorge Luis Gonzalez Hernandez <glz_jorge@hotmail.com>
2021-07-12 00:53:47 -0600

    Arreglar lo de cors y problema de ip del cliente

--- server/main.js
+++ server/main.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+//var io = require('socket.io')(server);
+var io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods:['get'],
+        allowedHeaders: ["*"],
+    }
+});
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 var messages = [{
     id: 1,
     texto: "Hola soy un mensaje",
@@ -24,6 +40,6 @@ io.on('connection', function(socket){
 });
 
 server.listen(3002, function(){
-    console.log("El servidor esta corriendo en http://localhost:3002");
+    console.log("El servidor esta corriendo en 3002");
 
 });
